b3012c6bfa89ca719897f858f8b00da02052df75
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-03 11:29:41 +0200

    improve validate middleware

--- src/validate.ts
+++ src/validate.ts
@@ -2,54 +2,39 @@ import { Request, Response, NextFunction } from "express";
 import { plainToClass } from "class-transformer";
 import { validate } from "class-validator";
 
+export function removeEmptyKeys(o) {
+  for (const key of Object.keys(o)) {
+    if (o[key] === undefined) {
+      delete o[key];
+    }
+  }
+}
+
 export function validateMiddleware<T>(dtoClass: new () => T) {
   return (req: Request, res: Response, next: NextFunction) => {
     (async () => {
       try {
-        if (req.body instanceof Array) {
-          const output: T[] = plainToClass<T, Object[]>(dtoClass, req.body, {
-            excludeExtraneousValues: true,
-          });
-          const validationErrors = await validate(output, {
-            skipMissingProperties: true,
-          });
-          if (validationErrors.length > 0) {
-            return res
-              .status(400)
-              .json(validationErrors);
-          }
-          // delete empty keys
-          output.forEach((o) => {
-            for (const key of Object.keys(o)) {
-              if (o[key] === undefined) {
-                delete o[key];
-              }
-            }
-          });
-          req.body = output;
-          return next();
+        const output: T[] | T =
+          req.body instanceof Array
+            ? plainToClass<T, Object[]>(dtoClass, req.body, {
+                excludeExtraneousValues: true,
+              })
+            : plainToClass<T, any>(dtoClass, req.body, {
+                excludeExtraneousValues: true,
+              });
+
+        const validationErrors = await validate(output, {
+          skipMissingProperties: true,
+        });
+        if (validationErrors.length > 0) {
+          return res.status(400).json(validationErrors);
         }
-        if (req.body instanceof Object) {
-          const output: T = plainToClass<T, any>(dtoClass, req.body, {
-            excludeExtraneousValues: true,
-          });
-          const validationErrors = await validate(output, {
-            skipMissingProperties: true,
-          });
-          if (validationErrors.length > 0) {
-            return res
-              .status(400)
-              .json(validationErrors);
-          }
-          // delete empty keys
-          for (const key of Object.keys(output)) {
-            if (output[key] === undefined) {
-              delete output[key];
-            }
-          }
-          req.body = output;
-          return next();
-        }
+        output instanceof Array
+          ? output.forEach(removeEmptyKeys)
+          : removeEmptyKeys(output);
+
+        req.body = output;
+        return next();
       } catch (e) {
         console.log("e: ", e);
         res.status(500).end();
