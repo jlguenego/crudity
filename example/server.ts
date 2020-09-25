68ce964c054b6a520318503e927a6ef7893f857b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-09-25 18:28:36 +0200

    updated interface

--- example/server.ts
+++ example/server.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import path from "path";
-import { Crudity } from "../src";
+import { crudity } from "../src";
 import { Article } from "./article.dto";
 
 const app = express();
 const filename = path.resolve(__dirname, "../data/test.json");
-const articleRouter = new Crudity<Article>({ filename, dtoClass: Article })
-  .router;
 
 app.use(express.json());
-app.use("/ws/articles", articleRouter);
+app.use(
+  "/ws/articles",
+  crudity<Article>({ filename, dtoClass: Article })
+);
 
 app.listen(3000, () => console.log("Server started on port 3000"));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import { Crudity } from "./Crudity";
+import { CrudityOptions } from "./CrudityOptions";
+
 export * from "./Crudity";
 export * from "./CrudityOptions";
+
+export function crudity<T>(opts: CrudityOptions<T>) {
+    return new Crudity<T>(opts).router;
+}
