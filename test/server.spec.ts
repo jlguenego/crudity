a7ec53d505d985f48a85d607e1277c3762659ee9
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-05 17:21:07 +0100

    test: server start and stop

--- test/server.spec.ts
+++ test/server.spec.ts
@@ -1,10 +1,21 @@
-import {Person} from './misc/Person';
+import fs from 'fs';
+import {WebServer} from './../src/WebServer';
+import got from 'got';
 import assert from 'assert';
 
+const port = +(process.env.TEST_PORT || 3333);
+
 describe('Server', () => {
-  it('should test', () => {
-    const alice = new Person();
-    alice.sayHello();
-    assert(true);
+  it('should start and stop', async () => {
+    const webServer = new WebServer({
+      port,
+    });
+    await webServer.start();
+    const text = await got.get(`http://localhost:${port}/test.txt`).text();
+    const content = await fs.promises.readFile('./public/test.txt', {
+      encoding: 'utf-8',
+    });
+    await webServer.stop();
+    assert.deepStrictEqual(text, content);
   });
 });
