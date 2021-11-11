5dc78cf68b6235faa75eeb6a2a8a8598065f1cb8
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-11 12:51:35 +0100

    dev: collection

--- src/CRUDService/mongodb/MongoDBCRUDService.ts
+++ src/CRUDService/mongodb/MongoDBCRUDService.ts
@@ -7,6 +7,7 @@ import {renameId, renameIdForArray} from './utils';
 
 export class MongoDBCRUDService<T extends Idable> extends CRUDService<T> {
   client = new MongoClient(this.options.uri, this.options.opts);
+  collection = this.client.db().collection(this.resourceName);
 
   constructor(resourceName: string, public options: MongoDBStorageOptions) {
     super(resourceName);
@@ -28,21 +29,14 @@ export class MongoDBCRUDService<T extends Idable> extends CRUDService<T> {
     query: CrudityQueryString,
     pageSize: number
   ): Promise<Partial<T>[]> {
-    const result = await this.client
-      .db()
-      .collection(this.resourceName)
-      .find({})
-      .toArray();
+    const result = await this.collection.find({}).toArray();
     console.log('result: ', result);
     return renameIdForArray<T>(result);
   }
 
   async getOne(id: string): Promise<T | undefined> {
     const objectId = new ObjectId(id);
-    const result = await this.client
-      .db()
-      .collection(this.resourceName)
-      .findOne({_id: objectId});
+    const result = await this.collection.findOne({_id: objectId});
     console.log('result: ', result);
     if (!result) {
       throw new Error('not found');
@@ -61,7 +55,7 @@ export class MongoDBCRUDService<T extends Idable> extends CRUDService<T> {
   }
 
   async removeAll(): Promise<void> {
-    await this.client.db().collection(this.resourceName).deleteMany({});
+    await this.collection.deleteMany({});
   }
 
   rewrite(arg0: T): Promise<T> {
