2f98d60a5e2c5c13c2b6544ea93beb7b1109461f
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-03 14:52:48 +0100

    dev: added doc

--- src/interfaces/CrudityOptions.ts
+++ src/interfaces/CrudityOptions.ts
@@ -1,8 +1,26 @@
 import {MongoClientOptions} from 'mongodb';
 
 export interface CrudityOptions {
+  /**
+   * Size for pagination.
+   *
+   * @default 15
+   */
   pageSize: number;
+  /**
+   * Storage options object (file storage or mongodb storage)
+   *
+   * @default {
+   *   type: 'file',
+   *   dataDir: './data',
+   * }
+   */
   storage: StorageOptions;
+  /**
+   * milliseconds of time before sending back an HTTP response (for testing purpose).
+   *
+   * @default 0
+   */
   delay: number;
 }
 
@@ -10,11 +28,22 @@ export type StorageOptions = FileStorageOptions | MongoDBStorageOptions;
 
 export interface FileStorageOptions {
   type: 'file';
+  /**
+   * Directory where to store the file. The filename will be <resourcename>.json.
+   */
   dataDir: string;
 }
 
 export interface MongoDBStorageOptions {
   type: 'mongodb';
+
+  /**
+   * MongoDB URI connection string.
+   */
   uri: string;
+
+  /**
+   * MongoClientOptions. See the MongoClient NodeJS drivers options.
+   */
   opts?: MongoClientOptions;
 }
