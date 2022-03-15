af03618e370b4d17f542d9425c974a75c6d77f6a
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-15 15:23:04 +0100

    dev: added info to openapi doc

--- src/interfaces/OpenApi3.ts
+++ src/interfaces/OpenApi3.ts
@@ -2,20 +2,67 @@ export type SemVer = `${number}.${number}.${number}`;
 
 export interface OpenApi3 {
   openapi: SemVer;
-  info?: {
-    title: string;
-    version: string;
-    description: string;
-  };
-  servers?: {
-    url: string;
-    description: string;
-  }[];
-  paths: {
-    [path: string]: {
-      [method: string]: {
-        description: string;
-      };
+  info: InfoObject;
+  servers?: ServerObject[];
+  paths: PathsObject;
+  components?: ComponentsObject;
+  security?: SecurityRequirementObject[];
+  tags?: TagObject[];
+  externalDocs?: ExternalDocumentationObject;
+}
+
+export interface InfoObject {
+  title: string;
+  description?: string;
+  termsOfService?: string;
+  contact?: ContactObject;
+  license?: LicenseObject;
+  version: string;
+}
+
+export interface ServerObject {
+  url: string;
+  description: string;
+}
+
+export interface PathsObject {
+  [path: string]: {
+    [method: string]: {
+      description: string;
     };
   };
+}
+
+export interface ComponentsObject {
+  schemas?: { [name: string]: SchemaObject };
+}
+
+export interface SecurityRequirementObject {
+  [name: string]: string[];
+}
+
+export interface TagObject {
+  name: string;
+  description?: string;
+  externalDocs: ExternalDocumentationObject;
+}
+
+export interface SchemaObject {
+  [name: string]: unknown;
+}
+
+export interface ExternalDocumentationObject {
+  description?: string;
+  url: string;
+}
+
+export interface ContactObject {
+  name?: string;
+  url?: string;
+  email?: string;
+}
+
+export interface LicenseObject {
+  name: string;
+  url?: string;
 }
